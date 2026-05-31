6e037eb933db3e62a98bf7e8b99f1909892b035a
pupupu-qqq <pupupu-qqq@users.noreply.github.com>
2026-05-31 21:51:37 +0300

    Fix missing review id validation

--- src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/ReviewService.java
@@ -107,10 +107,10 @@ public class ReviewService {
         if (review.getIsPositive() == null) {
             throw new ValidationException("Review positive flag is not passed");
         }
-        if (review.getUserId() <= 0) {
+        if (review.getUserId() == 0) {
             throw new ValidationException("Review user id is not passed");
         }
-        if (review.getFilmId() <= 0) {
+        if (review.getFilmId() == 0) {
             throw new ValidationException("Review film id is not passed");
         }
     }
